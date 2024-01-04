08718f11e6599d73f658e65a5b817b5cd67a8c5a
kekehurry <arhukai1111@gmail.com>
2024-01-04 10:39:44 -0500

    remove .next

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/kai/Library/CloudStorage/GoogleDrive-kaihu@media.mit.edu/My Drive/BDI/pud-ui/src/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ic/cTZL+DlSDwT86CscUKClUiwrV8CYANpzTiJddu2I=\"\n}"
